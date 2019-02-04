b043d1e982a0c5b50acf9cb380f0e726daa3ae38
Çağatay Ilgar <46165145+Urban-Monk@users.noreply.github.com>
2019-02-04 17:07:42 +0100

    Update ThisProper.java

--- ThisProper.java
+++ ThisProper.java
@@ -4,19 +4,21 @@ class Dog2 {
 	String color = "white";
 	
 	public Dog2(int age, String color) {
-		this.age = age;						// "THIS" ist eine Referenzvariable für das jeweilige Objekt. Ein Platzhalter quasi.
+		// "THIS" ist eine Referenzvariable für das jeweilige Objekt. Ein Platzhalter quasi.
+		this.age = age;						
 		this.color = color;
 	}
 	
 	public void dispAttr() {
-		StdOut.println("Dog's age: " + age + "\nDog's color: " + color);		// wir könnten auch this.age bzw. this.color schreiben aber java macht es automatisch für uns.
+		// wir könnten auch this.age bzw. this.color schreiben aber java macht es automatisch für uns.
+		StdOut.println("Dog's age: " + age + "\nDog's color: " + color);		
 	}
 }
 public class ThisProper {
 	public static void main(String args[]) {
 		// Neues Dog-Objekt erzeugen:
-		
-		Dog2 max = new Dog2(5, "black");										// Die Werte 2 und black verdecken sozusagen die oben in der Klasse deklarierten Werte.
+		// Die Werte 2 und black verdecken sozusagen die oben in der Klasse deklarierten Werte.
+		Dog2 max = new Dog2(5, "black");										
 		max.dispAttr();
 	}
 }
